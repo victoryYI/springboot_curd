a89bc84cf0df799a670d12ba8a0d171e3703b40d
易胜利 <yisl@YSL-MBP.local>
2019-01-04 15:33:02 +0800

    Initial commit

---
+++ src/main/java/com/asiainfo/springboot_curd/SpringbootCurdApplication.java
@@ -0,0 +1,14 @@
+package com.asiainfo.springboot_curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCurdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCurdApplication.class, args);
+    }
+
+}
+
